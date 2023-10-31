4cef45ac2aa0378b26efe61d4a9a5a749731e57d
Arturo Chavarria <chavarriavillareviajorgearturo@gmail.com>
2023-10-30 23:46:34 -0600

    Agregamos el ranking y los libros

--- Apis/CompararFrase.cpp
+++ Apis/CompararFrase.cpp
@@ -64,7 +64,7 @@ class CompararFrase{
             //llamo al arbol
             BinaryTree tree;
             
-
+            //fileNames es un vector con los títulos de los libros
             for (const auto& fileName : fileNames) {
                 //ya tengo los titulos de los libros
                 for (TemaLibro libro : temasLibro)
@@ -78,16 +78,44 @@ class CompararFrase{
                     }
                 }
             }
-            
 
-            //ahora tengo la frase en palabras claves y tengo el arbol
+            //Crea un hashtable donde se guarda el ranking
+            std::unordered_map<std::string, int> tablaRespuestas;
 
+            //Recorre un vector de palabras a buscar para ir revisando si hay comparaciones y poder sacar un ranking
+            //basado en la cantidad de apariciones que tuvo un libro según el vector de frases dadas
+            for (const auto& palabra : pfrase)
+            {   
+                //Se busca la palabra en el arbol para ver si existe, si no existe una de las palabras en el arbol
+                //no sucede nada y el for sigue corriendo
+                nodo* resultado = tree.buscar(palabra);
+                if(resultado != nullptr)
+                {
+                    //Revisa si la palabra se encuentra dentro de la tabla de hash, en caso de que sí, se le
+                    //aumenta al valor de apariciones al libro en 1, en caso de que no exista se crea un
+                    //nuevo elemento en el hash con el título del libro como key y 1 como value
+                    if(tablaRespuestas.find(resultado->bookTitle)) {
+                        tablaRespuestas[resultado->bookTitle]++;
+                    } else 
+                    {
+                        tablaRespuestas[resultado->bookTitle] = 1;
+                    }
+                    
+                }
+            }
 
+            // Crear un vector de pares clave-valor para almacenar elementos del hashtable
+            std::map<std::string, int, std::greater<>> tablaRespuestas;
 
+            // Copiar los elementos del hashtable al vector
+            for (const auto& pair : tablaRespuestas) {
+                // 'pair.first' es la clave (string)
+                std::string clave = pair.first;
+                // 'pair.second' es el vector correspondiente a la clave
+                int numero = pair.second;
 
+                tablaRespuestas[clave] = numero;
+            }
 
-            
-            
         }
-
 };
